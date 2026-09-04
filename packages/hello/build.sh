3d2e783c38af30083165fd53139e8e98a70f45ca
Natarizki <natakeren890@gmail.com>
2026-09-04 12:33:37 +0700

    Fix: match folder name to type value (binary, not bin)

--- packages/hello/build.sh
+++ packages/hello/build.sh
@@ -9,7 +9,7 @@ NAME="hello"
 VERSION="2.12.1"
 OUT_DIR="$1"   # passed by the workflow
 
-mkdir -p "$OUT_DIR/bin"
+mkdir -p "$OUT_DIR/binary"
 
 wget -nc "https://ftp.gnu.org/gnu/hello/hello-${VERSION}.tar.gz"
 tar -xf "hello-${VERSION}.tar.gz"
@@ -18,7 +18,7 @@ cd "hello-${VERSION}"
 ./configure --prefix=/usr
 make -j"$(nproc)"
 
-cp -v hello "$OUT_DIR/bin/hello"
+cp -v hello "$OUT_DIR/binary/hello"
 
 cat > "$OUT_DIR/.kpkmeta" << EOF
 {
